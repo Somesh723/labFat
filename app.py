6fe8a61e42c471b2ac5639b9a7a5458977ca76ce
Somesh723 <93442628+Somesh723@users.noreply.github.com>
2024-04-29 19:05:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("This is just a demmo code for out analysis on how pylint works")
+def add_numbers(a, b):
+  """Adds two numbers together.
+
+  Args:
+    a: The first number.
+    b: The second number.
+
+  Returns:
+    The sum of the two numbers.
+  """
+  return a + b
+
+
+# This code passes the super linter test.
+print(add_numbers(1, 2))
